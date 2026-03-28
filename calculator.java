2a7aa0422d761efa87bec573eed432d21a56dc90
Gunndeep Kaur <gunndeepkaur3107@gmail.com>
2026-03-28 08:29:39 +0530

    add calculator program in java

---
+++ calculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.print("Choose operation (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        double result;
+
+        if (op == '+') {
+            result = num1 + num2;
+        } else if (op == '-') {
+            result = num1 - num2;
+        } else if (op == '*') {
+            result = num1 * num2;
+        } else if (op == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                System.out.println("Cannot divide by zero");
+                return;
+            }
+        } else {
+            System.out.println("Invalid operator");
+            return;
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
+
